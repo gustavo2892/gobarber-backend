cde028242380fb8b28f9745af235b476068d0280
gustavo2892 <gustavo2892@gmail.com>
2020-01-15 13:14:39 -0300

    Config nodemailer and template

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: '',
-  port: '',
+  host: 'smtp.mailtrap.io',
+  port: 2525,
   secure: false,
   auth: {
-    user: '',
-    pass: '',
+    user: '8e09a7495d0588',
+    pass: '5411dbfa6d481f',
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
